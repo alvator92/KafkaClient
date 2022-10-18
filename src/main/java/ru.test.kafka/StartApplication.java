cc52f334e553b8f6156fd231d3e8bf6c5cc00edb
alvator92 <alvator@list.ru>
2022-10-18 16:49:59 +0300

    add pom.xml dependencies

---
+++ src/main/java/ru.test.kafka/StartApplication.java
@@ -0,0 +1,14 @@
+package ru.test.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class StartApplication {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(StartApplication.class, args);
+        System.out.println("Hello LoadGen!");
+
+    }
+}
