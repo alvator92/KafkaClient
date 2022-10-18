2d57270328bca06bb6747c1947ff9be461c886f1
alvator92 <alvator@list.ru>
2022-10-18 17:07:39 +0300

    add application.yaml add DB configuration in HibernateConfig

---
+++ src/main/java/ru/test/kafka/configuration/JpaConfig.java
@@ -0,0 +1,20 @@
+package ru.test.kafka.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+/**
+ * Class for {@link }
+ *
+ * @author Vladislav K.
+ * @version 1.0
+ */
+
+@Configuration
+//@EnableJpaRepositories( basePackages = {"ru.test.kafka.service.dao.repository"},
+//        entityManagerFactoryRef = "apossEntityManager",
+//        transactionManagerRef = "apossTransactionManager")
+public class JpaConfig {
+
+}
